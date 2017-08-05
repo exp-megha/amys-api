72dfb335d3c639cd559e633280eacc7c0cbdf7a5
megha <megha.s@experionglobal.com>
2017-08-05 13:07:13 +0530

    url updated

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "0.0.1",
   "description": "Documentation of No2fyme APIs for Mobile and Web",
   "title": "API Documentation for No2fyme",
-  "url": "http://52.15.243.102:3000",
+  "url": "https://amys-api.herokuapp.com/",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-08-05T06:59:42.946Z",
+    "time": "2017-08-05T07:26:59.064Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
