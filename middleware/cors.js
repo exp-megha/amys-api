09d91eb77acf335a59ea3b57732c333d876578a8
Megha <megha.s@experionglobal.com>
2017-08-15 20:42:58 +0530

    auth added in headers

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,8 +1,8 @@
 var cors = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Expose-Headers', 'x-auth');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, x-auth, platform');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, x-auth, platform, Auth');
     res.header('Access-Control-Allow-Credentials', 'true');
 
     if (req.method === 'OPTIONS') {
