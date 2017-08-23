592721b51fc89d446d5e62a972bb962735f53582
Megha <megha.s@experionglobal.com>
2017-08-23 11:02:16 +0530

    settings updated

--- modules/users/users.controller.js
+++ modules/users/users.controller.js
@@ -5,7 +5,7 @@ var searchAndFilters = require('./../../utils/searchAndFilters');
 var constants = require('./../../constants');
 var ObjectId = require('mongodb').ObjectId;
 var changeCase = require('change-case');
-
+var async = require('async');
 /**
  * function to invoke the get Provider details functionality
  * @author Megha
@@ -13,12 +13,21 @@ var changeCase = require('change-case');
  * @createdOn 09-May-2017
  */
 let applicationSettings = (req, res) => {
+    let settings_array = [];
     ApplicationSetting.find({})
         .then((settings) => {
             if (!settings) {
                 return Promise.reject('no-records-found');
             }
-            return res.status(200).message('Application-settings-retrieved-successfully').returnSuccess(settings);
+            async.eachSeries(settings, function (data, callback) {
+                let new_obj = {};
+                new_obj[data.settings_name] = data.settings_value;
+                // console.log('new_obj: ',new_obj);
+                settings_array.push(new_obj);
+                callback();
+            }, function () {
+                return res.status(200).message('Application-settings-retrieved-successfully').returnSuccess(settings_array);
+            });
         }).catch((err) => {
             res.status(400).message(err).returnFailure(null);
         });
